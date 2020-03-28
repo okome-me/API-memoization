a27a2b5bc87ec6cb56632701d09eeb606f70c3c6
nobori-urume <urume@nobori.ltd>
2020-03-29 00:02:58 +0900

    abc082_b

---
+++ src/app/atcorder/abc082_b/Main.java
@@ -0,0 +1,21 @@
+package app.atcorder.abc082_b;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] s = sc.nextLine().toCharArray();
+		char[] t = sc.nextLine().toCharArray();
+        Arrays.sort(s);
+        Arrays.sort(t);
+
+        StringBuilder revercseT = new StringBuilder();
+		revercseT.append(t);
+        revercseT.reverse();
+
+        System.out.println(String.valueOf(s).compareTo(revercseT.toString()) < 0 ? "Yes" : "No");
+    }
+}
---
+++ src/app/atcorder/abc082_b/problem_statement.md
@@ -0,0 +1,5 @@
+## 概要
+文字列比較、ソート
+
+## Link
+https://atcoder.jp/contests/abc082/tasks/abc082_b
