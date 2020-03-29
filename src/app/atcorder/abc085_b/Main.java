e332cc2aa5715f80d13e1ce5bb964333beee4fff
nobori-urume <urume@nobori.ltd>
2020-03-29 18:34:02 +0900

    abc085_b

---
+++ src/app/atcorder/abc085_b/Main.java
@@ -0,0 +1,28 @@
+package app.atcorder.abc085_b;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] d = new int[N];
+        for (int i = 0; i < N; i++) {
+            d[i] = sc.nextInt();
+        }
+        Arrays.sort(d);
+
+        int sheets = 0;
+        for (int i = 0; i < N; i++) {
+            if (i==0 || d[N-1-i]<d[N-i]) {
+                sheets++;
+            }
+        }
+
+        System.out.printf("%d\n", sheets);
+        sc.close();
+    }
+}
