81e99b619bf6a37ee817e07df9a2a80d1ab1e07b
nobori-urume <urume@nobori.ltd>
2020-03-29 20:20:46 +0900

    fix: abc085_c

--- src/app/atcorder/abc085_c/Main.java
+++ src/app/atcorder/abc085_c/Main.java
@@ -14,16 +14,15 @@ public class Main {
         int _1000 = -1;
 
         for (int i = 0; i <= N; i++) {
-            for (int j = 0; j <= N-i; j++) {
-                for (int k = 0; k <= N-i-j; k++) {
-                    if (i + j + k == N && 10000 * i + 5000 * j + 1000 * k == Y) {
-                        _10000 = i;
-                        _5000 = j;
-                        _1000 = k;
-                        System.out.printf("%d %d %d\n", _10000, _5000, _1000);
-                        sc.close();
-                        return;
-                    }
+            for (int j = 0; j <= N - i; j++) {
+                int k = N - i - j;
+                if (i + j + k == N && 10000 * i + 5000 * j + 1000 * k == Y) {
+                    _10000 = i;
+                    _5000 = j;
+                    _1000 = k;
+                    System.out.printf("%d %d %d\n", _10000, _5000, _1000);
+                    sc.close();
+                    return;
                 }
             }
         }
