61530b48e04402e306e6a25cdb5acecaf7bc2f07
nobori-urume <urume@nobori.ltd>
2020-03-29 22:09:25 +0900

    arc089_a

---
+++ src/app/atcorder/arc089_a/Main.java
@@ -0,0 +1,32 @@
+package app.atcorder.arc089_a;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] t = new int[N+1];
+        int[] x = new int[N+1];
+        int[] y = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            t[i] = sc.nextInt();
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        sc.close();
+
+        for (int i = 1; i <= N; i++) {
+            int load = Math.abs(x[i] - x[i - 1]) + Math.abs(y[i] - y[i - 1]);
+            int time = t[i] - t[i - 1];
+            if (time < load || load % 2 != time % 2) {
+                System.out.println("No");
+                return;
+            }
+        }
+
+        System.out.println("Yes");
+    }
+}
---
+++ src/app/atcorder/arc089_a/problem_statement.md
@@ -0,0 +1,2 @@
+## Link
+https://atcoder.jp/contests/abc086/tasks/arc089_a
